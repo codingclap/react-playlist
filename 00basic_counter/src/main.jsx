8bc3b2fb74c7395218777f4ae3f3c24e89ab1a9c
codingclap <codingclap@gmail.com>
2024-10-22 16:37:33 +0530

    props v2

--- 00basic_counter/src/main.jsx
+++ 00basic_counter/src/main.jsx
@@ -9,8 +9,8 @@ createRoot(document.getElementById('root')).render(
     <Count />
 
     <h2 className="text-center text-white text-4xl mt-20">2. React - Props</h2>
-    <Card heading="Best Technology of the year 2024"/>
-    <Card heading="Next js is a best Framework in 2024"/>
-    
+    <Card heading="Best Technology of the year 2024" btnText="Visit Here!"/>
+    <Card heading="Next js is a best Framework in 2024" btnText="Click Here!"/>
+
   </StrictMode> 
 )
