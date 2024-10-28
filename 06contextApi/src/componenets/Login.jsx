e06d295554754244629444bbea757bba0d0f34b6
codingclap <codingclap@gmail.com>
2024-10-28 11:32:26 +0530

    context-api

--- 06contextApi/src/componenets/Login.jsx
+++ 06contextApi/src/componenets/Login.jsx
@@ -4,23 +4,23 @@ import UserContext from '../context/UserContext';
 
 
 function Login() {
+ 
+    const [username, setName] = useState()
+    const [password, setPass] = useState()
+    const {setUser} = useContext(UserContext);          // setUser Accept Data here after submitting
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    const {setUser} =  useContext(UserContext);         // setUser Accept Data here after submitting
-
-    const SubmitHandler = (e) => {
+    function SubmitHandler(e){
         e.preventDefault();
-        setUser({username, password})            // Sending data to the UserContext
+        setUser({username, password})               // Sending data to the UserContext
+
     }
 
     return (
         <>
             <div>
                 <h2>Login</h2>
-                <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
-                <input type="text" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <input type="text" placeholder="Username" value={username} onChange={(e) => setName(e.target.value)}/>
+                <input type="text" placeholder="password" value={password} onChange={(e) => setPass(e.target.value)}/>
                 <button onClick={SubmitHandler}>Submit</button>
             </div>
         </>
