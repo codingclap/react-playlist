f3eee39dfc7f2e8c9935c2d396f01772fc6882d9
Anuj Kumar <86232374+codingclap@users.noreply.github.com>
2024-12-12 18:49:01 +0530

    new Project done

---
+++ 11.blogging/src/App.jsx
@@ -0,0 +1,13 @@
+ 
+function App() {
+   
+
+  return (
+    <>
+        <h1>Blog App with Appwrite</h1>
+        { console.log(process.env.REACT_APP_APPWITE_URL) }
+    </>
+  )
+}
+
+export default App
